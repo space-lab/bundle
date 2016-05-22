0135ce76bddf61a9980b8d11b03177bb5540bc68
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2016-05-22 12:57:37 +0800

    separate modal component and put changecollection inside it

---
+++ src/components/Modal/index.js
@@ -0,0 +1,18 @@
+import './index.css'
+
+export default class Modal extends React.Component {
+  static propTypes = {
+    position: React.PropTypes.object,
+    className: React.PropTypes.string
+  }
+
+  render () {
+    let { children, style, className } = this.props
+
+    return (
+      <div className={'modal ' + className} style={style}>
+        {children}
+      </div>
+    )
+  }
+}
