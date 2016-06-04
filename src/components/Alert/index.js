add8247eef5a9794b0872d7cd1b8c98298640432
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2016-06-04 08:54:57 +0545

    dismiss alert after 5sec

--- src/components/Alert/index.js
+++ src/components/Alert/index.js
@@ -1,29 +1,36 @@
 import ImmutablePropTypes from 'react-immutable-proptypes'
 import './index.css'
 
-function alertContent (alerts) {
-  if (alerts.size === 1) {
-    return alerts
-  } else {
-    return <ul>{alerts.map((item, key) => <li key={key}>{item}</li>)}</ul>
+export default class Alert extends React.Component {
+  static propTypes = {
+    type: React.PropTypes.string.isRequired,
+    alerts: ImmutablePropTypes.list.isRequired,
+    removeAlert: React.PropTypes.func.isRequired
+  }
+
+  componentDidMount () {
+    setTimeout(this.props.removeAlert, 5000)
+  }
+
+  renderContent (alerts) {
+    if (alerts.size === 1) {
+      return alerts
+    } else {
+      return <ul>{alerts.map((item, key) => <li key={key}>{item}</li>)}</ul>
+    }
+  }
+
+  render () {
+    const { type, alerts, removeAlert } = this.props
+
+    return (
+      <div className='alerts-container'>
+        <div className={`alert alert-${type}`}>
+          {this.renderContent(alerts)}
+          <div className='close-icon' onClick={removeAlert}/>
+        </div>
+      </div>
+    )
   }
 }
 
-export default function Alert ({ type, alerts, removeAlert }) {
-  const content = alertContent(alerts)
-
-  return (
-    <div className='alerts-container'>
-      <div className={`alert alert-${type}`}>
-        {content}
-        <div className='close-icon' onClick={removeAlert}/>
-      </div>
-    </div>
-  )
-}
-
-Alert.propTypes = {
-  type: React.PropTypes.string.isRequired,
-  alerts: ImmutablePropTypes.list.isRequired,
-  removeAlert: React.PropTypes.func.isRequired
-}
