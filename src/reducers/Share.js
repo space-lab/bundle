039da45d6e856bc5a542dbbd01f7ebc5869ffa84
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2016-06-15 01:37:47 +0545

    changes in reducers

--- src/reducers/Share.js
+++ src/reducers/Share.js
@@ -7,9 +7,8 @@ let defaultState = Map({
 export default function (state = defaultState, action) {
   switch (action.type) {
     case 'RECEIVE_SHARES':
-      action.shares.forEach(share => {
-        state = state.setIn(['byId', share.id], share)
-      })
+      action.shares.forEach(share =>
+        state = state.setIn(['byId', share.id], share))
 
       return state
 
