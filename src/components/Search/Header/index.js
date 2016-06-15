2ee5f23ad3a73eb34c5c6dd85c4c48049f2d890e
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2016-06-15 20:29:57 +0545

    capitalise directories

---
+++ src/components/Search/Header/index.js
@@ -0,0 +1,30 @@
+import { Link, browserHistory } from 'react-router'
+import './index.css'
+
+export default class SearchHeader extends React.Component {
+  static propTypes = {
+    query: React.PropTypes.string
+  }
+
+  onChange ({ target }) {
+    browserHistory.push(`/search/${target.value}`)
+  }
+
+  render () {
+    const { query } = this.props
+
+    return (
+      <div className='search-header-wrapper'>
+        <input
+          className='search-input'
+          autoFocus={true}
+          type='text'
+          placeholder='Search...'
+          onChange={this.onChange}
+          value={query || ''} />
+
+        <Link to='/bundles' className='icon close-icon'/>
+      </div>
+    )
+  }
+}
