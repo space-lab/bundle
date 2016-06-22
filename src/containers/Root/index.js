41ca4f84f7b0253ba24f3a45c53048ad84a9ee85
Davit Khaburdzania <davit.khaburdzania@gmail.com>
2016-06-22 09:34:52 +0400

    removed unused import

--- src/containers/Root/index.js
+++ src/containers/Root/index.js
@@ -2,7 +2,6 @@ import { Provider } from 'react-redux'
 
 import Routes from 'routes'
 import { store } from 'store/store'
-import { Share } from 'containers'
 
 export default function Root () {
   return (
