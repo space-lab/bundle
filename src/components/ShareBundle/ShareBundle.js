8d9b0f97b9e6955416303afd8c98dfae26828f11
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2016-07-07 09:46:47 +0400

    share bundle cleanups

--- src/components/ShareBundle/ShareBundle.js
+++ src/components/ShareBundle/ShareBundle.js
@@ -2,20 +2,12 @@ import ImmutablePropTypes from 'react-immutable-proptypes'
 import listensToClickOutside from 'react-onclickoutside/decorator'
 import ui from 'redux-ui'
 import { ShareResource } from 'components'
-
 import './ShareBundle.css'
 
-@ui({
-  state: { q: '', isOpen: false }
-})
+@ui({ state: { q: '', isOpen: false } })
 @listensToClickOutside()
 export default class ShareBundle extends React.Component {
-  static propTypes = {
-    resource: ImmutablePropTypes.record,
-    resourceName: React.PropTypes.string
-  }
-
-  handleClickOutside (e) {
+  handleClickOutside () {
     if (this.props.ui.isOpen) {
       this.props.updateUI('isOpen', false)
     }
@@ -26,14 +18,12 @@ export default class ShareBundle extends React.Component {
   }
 
   render () {
-    return (
-      <div className='share-bundle-wrapper'>
-        <button className='round-button' onClick={::this.openModal}>
-          Share
-        </button>
+    return <div className='share-bundle-wrapper'>
+      <button className='round-button' onClick={::this.openModal}>
+        Share
+      </button>
 
-        <ShareResource {...this.props} />
-      </div>
-    )
+      <ShareResource {...this.props} />
+    </div>
   }
 }
