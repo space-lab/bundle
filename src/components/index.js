6582a5269a56c21e8d1731d0805bc4c50ddc6d92
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2016-07-07 21:14:35 +0400

    moved around some code

--- src/components/index.js
+++ src/components/index.js
@@ -1,27 +1,40 @@
 export Menu from './Menu'
 export Alert from './Alert'
 export Modal from './Modal'
-export List from './List/List'
-export Link from './Link/Link'
 export DateTime from './DateTime'
-export UserMenu from './UserMenu'
-export ListItem from './ListItem'
 export Editable from './Editable'
-export UrlShare from './UrlShare'
-export LoadMore from './LoadMore'
+
+export UserMenu from './UserMenu'
 export * as Search from './Search'
-export Bundle from './Bundle/Bundle'
+
+export ResourceNavigation from './ResourceNavigation'
+
+// Wrapper Components
 export Header from './Header/Header'
-export Content from './Content/Content'
-export AddLink from './AddLink/AddLink'
-export ListToolbar from './ListToolbar'
-export Autocomplete from './Autocomplete'
-export NoBundle from './NoBundle/NoBundle'
-export EnterUrl from './EnterUrl/EnterUrl'
-export TagInput from './TagInput/TagInput'
-export ShareResource from './ShareResource'
-export ResourceFilters from './ResourceFilters'
 export JoinBundle from './JoinBundle/JoinBundle'
 export ChangeCollection from './ChangeCollection'
+
+export Content from './Content/Content'
+
+export Bundle   from './Bundle/Bundle'
+export EnterUrl from './Bundle/EnterUrl/EnterUrl'
+export AddLink  from './Bundle/AddLink/AddLink'
+export Link     from './Bundle/Link/Link'
+export NoBundle from './Bundle/NoBundle/NoBundle'
+
+// List Components
+export List        from './List/List'
+export ListItem    from './List/ListItem'
+export ListToolbar from './List/ListToolbar'
+
+// List Filters and Loaders
+export ResourceFilters from './ResourceFilters'
+export LoadMore from './LoadMore'
+
+export ShareResource from './ShareResource'
 export ShareBundle from './ShareBundle/ShareBundle'
-export ResourceNavigation from './ResourceNavigation'
+export UrlShare from './UrlShare'
+
+export Autocomplete from './Autocomplete'
+export TagInput from './TagInput/TagInput'
+
