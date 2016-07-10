4a0d85a167a7de2a66243f7e14cd6c9b47b63e42
Davit Khaburdzania <davit.khaburdzania@gmail.com>
2016-07-10 15:33:24 +0400

    changes in class name

--- src/components/Bundle/NoBundle/NoBundle.js
+++ src/components/Bundle/NoBundle/NoBundle.js
@@ -1,7 +1,7 @@
 import './NoBundle.css'
 
 const NoBundle = () =>
-  <div className='no-bundle-wrapper'>
+  <div className='no-bundle-component'>
     <div className='no-bundle-text'>No Bundle selected...</div>
   </div>
 
