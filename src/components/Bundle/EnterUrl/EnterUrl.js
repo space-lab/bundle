29a7dc4b60dd226d6ab497cb6921c8ddc2d63f10
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2016-08-01 16:27:02 +0400

    added isUrl validation onInput change when adding link

--- src/components/Bundle/EnterUrl/EnterUrl.js
+++ src/components/Bundle/EnterUrl/EnterUrl.js
@@ -1,3 +1,4 @@
+import { isUrl } from 'helpers'
 import './EnterUrl.css'
 
 export default class EnterUrl extends React.Component {
@@ -8,9 +9,9 @@ export default class EnterUrl extends React.Component {
     autoFocus: React.PropTypes.bool
   }
 
-  handleKeyPress (bundleId, handeUrlEnter, { target, key }) {
+  handleChange ({ target }) {
     let url = target.value
-    if (key === 'Enter') handeUrlEnter(url, bundleId)
+    if (isUrl(url)) this.props.handeUrlEnter(url, this.props.bundleId)
   }
 
   render () {
@@ -24,7 +25,7 @@ export default class EnterUrl extends React.Component {
           className='url-input'
           placeholder='Enter URL here and press Enter...'
           autoFocus={autoFocus}
-          onKeyPress={this.handleKeyPress.bind(this, bundleId, handeUrlEnter)}/>
+          onChange={::this.handleChange} />
       </div>
     )
   }
