030c716094e9a0c03e5ce434b781a99e9246aed6
Irakli Janiashvili <irakli.janiashvili@gmail.com>
2016-08-06 06:00:38 +0400

    share bubbles

--- src/components/Header/ShareBundle/ShareBundle.js
+++ src/components/Header/ShareBundle/ShareBundle.js
@@ -1,3 +1,4 @@
+import ImmutablePropTypes from 'react-immutable-proptypes'
 import { withState } from 'recompose'
 import { ShareResource, Permission } from 'components'
 import './ShareBundle.css'
@@ -9,18 +10,39 @@ let enhancer = withState('shareModal', 'updateShareModal', modalState)
 class ShareBundle extends React.Component {
   static propTypes = {
     shareModal: React.PropTypes.object.isRequired,
-    updateShareModal: React.PropTypes.func.isRequired
+    updateShareModal: React.PropTypes.func.isRequired,
+    resource: ImmutablePropTypes.record.isRequired,
+    users: ImmutablePropTypes.map.isRequired
   }
 
   render () {
-    return <div className='share-bundle-wrapper'>
-      <button className='main-button'
-        onClick={_ => this.props.updateShareModal({ isOpen: true })}>
-        Share
-      </button>
+    let props = this.props
+    let allUsers = props.resource.shares.map(share => share.user)
+      .unshift(props.users.get(props.resource.creator))
 
-      <Permission allow={this.props.shareModal.isOpen}>
-        <ShareResource {...this.props} />
+    let users = allUsers.take(5).reverse()
+    let moreUsers = allUsers.size - 5
+    console.log(moreUsers)
+
+    return <div className='share-bundle-wrapper'>
+      <div className='share-clicker' onClick={_ => props.updateShareModal({ isOpen: true })}>
+        <Permission allow={users.size > 1}>
+          <ul className='share-user-images'>
+            {users.map(user =>
+              <li key={user.id}><img src={user.image} /></li>
+            )}
+          </ul>
+        </Permission>
+
+        <Permission allow={moreUsers > 1}>
+          <span className='share-more-users'>and {moreUsers} more</span>
+        </Permission>
+
+        <button className='main-button'>Share</button>
+      </div>
+
+      <Permission allow={props.shareModal.isOpen}>
+        <ShareResource {...props} />
       </Permission>
     </div>
   }
